bc452136ee8c4e608c83ef9c93c32eecfd15d264
TRIABELLA <72142344+TRIABELLA@users.noreply.github.com>
2020-10-30 17:54:36 +0700

    Mengumpulkan tugas praktikum 6

---
+++ Tugas Web PHP/E41191205_Tria Bella Febrianti_Belajar PHP/echo.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<h2> BELAJAR PHP ITU MUDAH! </h2>";
+echo("Hello World <br>");
+echo "Aku sedang belajar PHP! <br>";
+echo "Ini ", "teks ", "yang ", "dibuat ", "terpisah ";
+
+?>
---
+++ Tugas Web PHP/E41191205_Tria Bella Febrianti_Belajar PHP/sintax_html_php.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo "Belajar PHP"; ?></title>
+</head> 
+<body>
+    <?php
+    echo "Saya sedang belajar PHP <br>";
+    echo "<p> Belajar PHP hingga jadi master</p>";
+    ?>
+</body>
+</html>
+
