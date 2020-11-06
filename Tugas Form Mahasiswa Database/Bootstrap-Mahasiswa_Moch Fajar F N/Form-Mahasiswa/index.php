624291dd3cd7124e613f13c98afebbf595e0b422
fajar098123 <fajar098123@gmail.com>
2020-11-07 00:47:16 +0700

    Mengumpulkan tugas Form Mahasiswa (Fajar)

---
+++ Tugas Form Mahasiswa Database/Bootstrap-Mahasiswa_Moch Fajar F N/Form-Mahasiswa/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+
+    <!-- Title Page-->
+    <title>Au Register Forms by Colorlib</title>
+
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
+</head>
+
+<body>
+    <div class="page-wrapper bg-gra-01 p-t-130 p-b-100 font-poppins">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">Form Mahasiswa</h2>
+                    <form action="target.php" method="POST">
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Nama Lengkap</label>
+                                    <input class="input--style-4" type="text" name="nama" onkeypress="return onlyAlphabets(event,this);" required>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">NIM</label>
+                                    <input class="input--style-4" type="text" name="nim" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Tanggal Lahir</label>
+                                    <div class="input-group-icon">
+                                        <input class="input--style-4 js-datepicker" type="text" name="birthday" required>
+                                        <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Jenis Kelamin</label>
+                                    <div class="p-t-10">
+                                        <label class="radio-container m-r-45">Laki laki
+                                            <input type="radio" checked="checked" name="gender" value="Laki laki">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                        <label class="radio-container">Perempuan
+                                            <input type="radio" name="gender" value="Perempuan">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Email</label>
+                                    <input class="input--style-4" type="email" name="email" required>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">No Telepon</label>
+                                    <input class="input--style-4" type="text" name="phone" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Username</label>
+                                    <input class="input--style-4" type="text" name="uname" required>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Password</label>
+                                    <input class="input--style-4" type="password" name="passw" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="p-t-15">
+                            <button class="btn btn--radius-2 btn--blue" type="submit" >Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+<?php
+include "kon.php";
+$name = @$_REQUEST['nama'];
+$nim = @$_REQUEST['nim'];
+$birthday = @$_REQUEST['birthday'];
+$gender = @$_REQUEST['gender'];
+$email = @$_REQUEST['email'];
+$phone = @$_REQUEST['phone'];
+$username = @$_REQUEST['uname'];
+$password = @$_REQUEST['passw'];
+$mysqli = "INSERT INTO tb_mahasiswa (nama,nim,birthday,gender,email,phone,uname,passw) VALUES ('$name','$nim','$birthday','$gender','$email','$phone','$username','$password')";
+$result = mysqli_query($conn,$mysqli);
+if ($result) {
+    echo "<script> alert ('Input Berhasil')</script>";
+} else {
+    echo "<script> alert ('Input Gagal')</script>";
+}
+mysqli_close($conn);
+?>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
+<!-- end document-->
