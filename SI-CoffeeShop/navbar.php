67473d52bc3f628ed8c312debb37308e9ef15b4f
fajar098123 <fajar098123@gmail.com>
2020-11-30 10:40:21 +0700

    Update navbar.php

--- SI-CoffeeShop/navbar.php
+++ SI-CoffeeShop/navbar.php
@@ -2,7 +2,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-    <a class="navbar-brand" href="#"><img src="img/logo1.png" class="logo" alt="..."></a>
+    <a class="navbar-brand" href="#"><img src="img/logo1" alt=""></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
