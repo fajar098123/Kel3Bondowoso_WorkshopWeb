f4eafb6dedc1278788e938ce65a8ed7a4d9c62b0
iphang190909 <iphang.admaja@gmail.com>
2020-12-14 15:32:29 +0700

    Update registrasi.php

--- SI-CoffeeShop/admin/dist/pages/akun/registrasi.php
+++ SI-CoffeeShop/admin/dist/pages/akun/registrasi.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:SI-CoffeeShop/admin/dist/pages/akun/registrasi.php
 <?php 
 include "../../../../config/connection.php";
 include "../../templates/header.php"
@@ -7,7 +6,7 @@ include "../../templates/header.php"
 <body class="sb-nav-fixed">
     <?php include '../../templates/topbar.php' ?>
     <?php include '../../templates/sidebar.php' ?>
-    
+
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
