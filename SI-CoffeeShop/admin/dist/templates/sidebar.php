4f8f4be3eabe6980f58cecb55426447094819b25
tegarshallahudin <tegarshallahudin1@users.noreply.github.com>
2020-12-15 01:01:36 +0700

    14/12/2020
    menambhkan database dan mengedit admin

--- SI-CoffeeShop/admin/dist/templates/sidebar.php
+++ SI-CoffeeShop/admin/dist/templates/sidebar.php
@@ -13,13 +13,27 @@
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Registrasi
                             </a>
+                            <a class="nav-link collapsed" href="<?= $_ENV['admin_base_url'] ?>dist/pages/akun/registrasi.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Daftar Akun
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Produk</div>
                             <a class="nav-link collapsed" href="<?= $_ENV['admin_base_url'] ?>dist/pages/produk/produk.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Produk
                             </a>
+                            <a class="nav-link collapsed" href="<?= $_ENV['admin_base_url'] ?>dist/pages/akun/registrasi.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Daftar Produk
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Artikel</div>
                             <a class="nav-link collapsed" href="<?= $_ENV['admin_base_url'] ?>dist/pages/artikel/artikel.php">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                                 Artikel
+                            </a>
+                            <a class="nav-link collapsed" href="<?= $_ENV['admin_base_url'] ?>dist/pages/artikel/artikel.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Daftar Artikel
                             </a>
                         </div>
                         </nav>
