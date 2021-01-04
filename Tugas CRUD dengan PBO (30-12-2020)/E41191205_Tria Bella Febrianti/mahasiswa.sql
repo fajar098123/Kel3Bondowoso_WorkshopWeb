bda56dae0ae44e694787e3b35151ef4db0530dc4
TRIABELLA <72142344+TRIABELLA@users.noreply.github.com>
2021-01-05 00:07:27 +0700

    Update mahasiswa.sql

--- Tugas CRUD dengan PBO (30-12-2020)/E41191205_Tria Bella Febrianti/mahasiswa.sql
+++ Tugas CRUD dengan PBO (30-12-2020)/E41191205_Tria Bella Febrianti/mahasiswa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 23, 2020 at 12:20 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.11
+-- Waktu pembuatan: 04 Jan 2021 pada 18.06
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mahasiswa`
+-- Struktur dari tabel `mahasiswa`
 --
 
 CREATE TABLE `mahasiswa` (
@@ -36,29 +36,18 @@ CREATE TABLE `mahasiswa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `mahasiswa`
+-- Dumping data untuk tabel `mahasiswa`
 --
 
 INSERT INTO `mahasiswa` (`nim`, `nama`, `jenis_kelamin`, `umur`) VALUES
-('E3190112', 'Kana Farira', 'Perempuan', 19),
-('E4119011', 'Franky', 'Laki-Laki', 19),
-('E41190112', 'Iqbal', 'Laki-Laki', 20),
-('E41190312', 'CVC', 'Laki-Laki', 19),
-('E41190322', 'PP', 'Perempuan', 12),
-('E41190331', 'Chooper', 'Laki-Laki', 18),
-('E41190333', 'Monkey D Luffy', 'Laki-Laki', 19),
-('E41190441', 'Igo', 'Laki-Laki', 19),
-('E41190442', 'Ensas', 'Laki-Laki', 19),
-('E41190457', 'Surya', 'Laki-Laki', 17),
-('E41190678', 'Doflaminggo', 'Perempuan', 19),
-('E41191553', 'Dicky Kurnia Ramadhan', 'Laki-Laki', 19);
+('E41191205', 'Tria Bella Febrianti', 'Perempuan', 19);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `mahasiswa`
+-- Indeks untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`nim`);
